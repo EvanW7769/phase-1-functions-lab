4a3a333240bed05d3599d973087729d2c2fb2723
Evan Watson <thomasevanwatson@gmail.com>
2022-02-23 12:21:34 -0500

    commit tree 767c456f0482de364d49a4d9b254b4f9d6c65b4d

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    return Math.abs(distance - 42)
+}
+
+function distanceFromHqInFeet(blocks){
+   return distanceFromHqInBlocks(blocks) * 264
+}
+
+function distanceTravelledInFeet(start, finish){
+  return(Math.abs(finish-start)*264)
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceTravelled = distanceTravelledInFeet(start, destination)
+    if(distanceTravelled<400){
+        return 0
+    }
+     else if (distanceTravelled > 400 && distanceTravelled < 2000 ){
+         return .02 * (distanceTravelled - 400)
+     }
+     else if (distanceTravelled >= 2000 && distanceTravelled < 2500){
+         return 25
+     }
+     else{
+         return "cannot travel that far"
+     }
+}
+
